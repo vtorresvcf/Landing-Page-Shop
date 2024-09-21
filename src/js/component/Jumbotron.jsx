ea8d10c2cd3dd66bd6b8ba1b57419db59f8a8677
Vicente Torres Luna <143291208+vtorresvcf@users.noreply.github.com>
2024-09-21 10:02:52 +0000

    terminado

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,8 +3,18 @@ import React from "react";
 const Jumbotron = () => {
   return (
     <div className="container jumbotron my-4 ">
-      <h1 className="text-center">Online store of products</h1>
-      
+      <h1 className="text-center ">Tienda online de productos tecnológicos</h1>
+      <p className="mt-4">
+        ¡Bienvenidos! Nos dedicamos a ofrecerte una experiencia de compra
+        excepcional con una amplia variedad de productos de alta calidad. Desde
+        artículos de moda y accesorios hasta productos tecnológicos y del hogar,
+        nuestro catálogo está cuidadosamente seleccionado para satisfacer tus
+        necesidades y gustos. Nuestra misión es brindarte productos innovadores
+        y de vanguardia, todo a precios competitivos. Nos enorgullecemos de
+        nuestra atención al cliente, asegurándonos de que cada compra sea fácil,
+        rápida y segura. Explora nuestras colecciones y descubre todo lo que
+        podemos ofrecerte. ¡Tu satisfacción es nuestra prioridad!
+      </p>
     </div>
   );
 };
